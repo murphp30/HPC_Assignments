9d87bb791144d8f593902e1043b45d757092df35
Pearse <murphp30@tcd.ie>
2017-12-13 17:55:38 +0000

    Finaly working

--- ex02/ex02sqrt.c
+++ ex02/ex02sqrt.c
@@ -14,17 +14,13 @@ int main( int argc, char *argv[] ) {
 		 return floor(pow(10,i)*n)/pow(10,i);
 	}	
 
-	//find first guess
 
 	
 	float i=1;
 	while( i <= no) {
-//	printf("i   = %f\n",i);
 		float sqno= pow(i, 2);
-//	printf("i^2 = %f\n", sqno);
 	
 		if ( sqno > no ) {
-//			printf("First guess is between %f and %f\n", i-1,i);
 			break;
 		}
 		else if ( sqno == no) {
@@ -38,32 +34,17 @@ int main( int argc, char *argv[] ) {
 
 	int j=1;
 	float nextguess;
-//	printf("First guess: %f\n", firstguess);
 	if ( sqno != no){
-	//	for( j=1; j<=decplace; j++){
 		
 		while (j<=decplace){
-			//printf ("Square of %f yields %f\n",firstguess, pow(firstguess,2));
 			if ( pow(firstguess,2) > no){
-//				printf("Square of %f=%f, Too high\n", firstguess, pow(firstguess,2));
-							
 				nextguess= firstguess-pow(10,-(decplace-(decplace-j)));	
-//				printf("Next guess: %f\n", nextguess);
-				//printf("Square of %f is %f\n", nextguess, pow(nextguess,2);
-
+				printf("Guess:%f  Square:%f\n", nextguess, pow(nextguess,2));
 				if (pow(nextguess,2) > no){
-//					printf("Square of %f=%f, still too high\n", nextguess, pow(nextguess,2));
 					firstguess=nextguess;
-//					printf("Next guess: %f\n", nextguess);
-
-				//	j--;
 				}
 				else if (pow(nextguess,2) < no){
 					nextguess= (firstguess+nextguess)/2;
-//					printf("Square of %f=%f too low now\n",nextguess, pow(nextguess,2));
-				//	printf("Next guess is %f\n", nextguess);
-				
-//					printf("Next guess: %f\n", nextguess);
 
 					firstguess=nextguess;
 					j++;
@@ -77,28 +58,19 @@ int main( int argc, char *argv[] ) {
 
 			}	
 			else if (  pow(firstguess,2) < no){
-//				printf("Square of %f=%f, Too low\n", firstguess, pow(firstguess,2));
 				float nextguess= firstguess+pow(10,-(decplace-(decplace-j)));
-			
-//				printf("Next guess: %f\n", nextguess);
-//				printf("Next guess is %f\n", nextguess);
+				printf("Guess:%f Square:%f\n", nextguess, pow(nextguess,2));
 				if (pow(nextguess,2) < no){
 					firstguess=nextguess;
-//					printf("Next guess: %f\n", nextguess);
-//					printf("Square of %f=%f, still too low\n", nextguess, pow(nextguess,2));
-					//j--;
 				}
 				else if (pow(nextguess,2) > no){
 					nextguess= (firstguess+nextguess)/2;
-//					printf("Square of %f=%f too high now\n",nextguess, pow(nextguess,2));
-//					printf("Next guess: %f\n", nextguess);
 					firstguess=nextguess;
 					j++;
 				}
 			//	else if (pow(nextguess,2) == no){
 			//		printf("Square root of %f is %f\n", no, nextguess);
 			//	}
-				//printf("Next guess is %f\n", nextguess);
 			
 			}
 		}
@@ -108,8 +80,6 @@ int main( int argc, char *argv[] ) {
 	else {
 		printf("Square root of %f is %f\n", no, i);
 	}
-//	float num=5.0;
-//	printf("%f to %d decimal places is %f\n", num, decplace, newPrecision(num, decplace));
 	return 0;
 
 }
